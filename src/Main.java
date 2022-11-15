312b1d3f8113948cb0cd797faaad075b11f634e8
irina.kustova <irenekitty@yandex.ru>
2022-11-15 10:16:47 +0300

    Homework completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задание 1");
+        String firstName = "Иван";
+        String miiddleName = "Семёнович";
+        String lastName = "Иванов";
+        String fullName = lastName + " " + firstName + " " + miiddleName;
+        System.out.println(fullName);
+        System.out.println("Задание 2");
+        String fullNameForReport = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullNameForReport);
+        System.out.println("Задание 3");
+        fullName = fullName.replace("ё", "e");
+        System.out.println(fullName);
+
     }
 }
